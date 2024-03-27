ca296c74600d52815220e76f3e6b04073ba819d7
KENK <ken1178776360@icloud.com>
2024-03-27 20:08:22 +0800

    new

--- objection/SettingViewController.swift
+++ objection/SettingViewController.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    //设置一个内含主要国家语言的数组：英语、日语、中文、法语
+    let languageArray = ["English", "Japanese", "Chinese"]
+    
+    //设置一个六面骰子数组，用于随机选择
+    let diceArray = ["1", "2", "3", "4", "5", "6"]
+}
+
